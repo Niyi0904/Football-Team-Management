8d182d3bb8911604a7084881775d61b79d0f8b67
Niyi0904 <owoyeminiyi2@gmail.com>
2026-02-11 13:13:36 +0100

    Changed Project meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='75' font-size='75' role='img' aria-label='football'>🏈</text></svg>",
+        url: "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='75' font-size='75' role='img' aria-label='soccer ball'>⚽</text></svg>",
         type: "image/svg+xml",
       }
     ]
